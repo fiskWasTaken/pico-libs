8d712950eb4389197629bc2d32151be8055788a8
fisk <fiskie@live.co.uk>
2020-01-02 15:37:29 +0000

    update vector funcs

--- vector-array.lua
+++ vector-array.lua
@@ -1,28 +1,49 @@
 -- vector.lua
 -- array notation
 
-vector={}
+vec={}
 
-function vector.dot(a,b)
+-- get the dot product of two vectors
+function vec.dot(a,b)
     return a[1]*b[1]+a[2]*b[2]
 end
 
-function vector.scl(a,m)
-    return a[1]*m,a[2]*m
+-- scale a vector
+function vec.scl(a,m)
+    return {a[1]*m,a[2]*m}
 end
 
-function vector.nor(a)
-    local len=vector.len(a[1],a[2])
+-- normalise a vector
+function vec.nor(a)
+    local len=vec.len(a)
     if len>0 then
-        return {a[1]/len, a[2]/len}
+        return {a[1]/len,a[2]/len}
     end
     return a
 end
 
-function vector.len(a)
+-- get the length of a vector
+function vec.len(a)
     return sqrt(abs(a[1])*2+abs(a[2])*2)
 end
 
-function vector.sub(a,b)
-    return a[1]-b[1],a[2]-b[2]
+-- set the length of a vector
+function vec.set_len(a,len)
+    local nor=vec.nor(a)
+    return vec.scl(nor,len)
+end
+
+-- sub vector 2 from vector 1
+function vec.sub(a,b)
+    return {a[1]-b[1],a[2]-b[2]}
+end
+
+-- add vector 2 to vector 1
+function vec.add(a,b)
+    return {a[1]+b[1],a[2]+b[2]}
+end
+
+-- get the vector for an angle
+function vec.from_angle(a)
+    return {sin(a),cos(a)}
 end

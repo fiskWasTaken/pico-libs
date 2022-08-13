2ebd874f0190c008ae85ba32d6412ce6e88bb862
fisk <git@fisk.cc>
2022-08-13 20:29:57 +0100

    add collision_diamond

--- collision_diamond.lua
+++ collision_diamond.lua
@@ -1,7 +1,7 @@
 -- simple helper for ap-based collision
 -- if the first flag (0x1) is set on a block, it will have collision.
 function collision_block(x,y,r)
- return collision(x-r,y-r) or collision(x-r,y+r) or collision(x+r,y-r) or collision(x+r,y+r)
+ return collision(x,y-r) or collision(x-r,y) or collision(x+r,y) or collision(x,y+r)
 end
 
 function collision(x,y)

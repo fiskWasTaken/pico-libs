6b353e0bf1820078540502716de46828565b35c3
fisk <git@fisk.cc>
2022-08-14 06:54:56 +0100

    add in_array

---
+++ in_array.lua
@@ -0,0 +1,6 @@
+-- return nil if not found in sequence
+function in_array(a,v)
+ for i in all(a) do
+  if (i==v) return i
+ end
+end
--- vector/len_cap.lua
+++ vector/len_cap.lua
@@ -3,6 +3,5 @@
 -- length may never be higher than about 181.0166, avoid using numbers larger than 100 for inputs
 function vec_len(x,y)
  local r=x*x+y*y
- if (r<0) r=0x7fff
- return sqrt(r)
+ return sqrt(r<0 and 0x7fff or r)
 end

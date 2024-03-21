5437b28740d672a6b3fa91accec0697fbc9cc783
fisk <git@fisk.cc>
2024-03-21 16:52:55 +0000

    feat: update collision.lua

--- collision.lua
+++ collision.lua
@@ -3,17 +3,17 @@
 
 -- get the normals for points
 function normals(points)
-local out={}
+ local out={}
 
-for i=1,#points do
-local curr=points[i]
-local nxt=points[(i%#points)+1]
+ for i=1,#points do
+  local curr=points[i]
+  local nxt=points[(i%#points)+1]
 
--- perpl
-out[i]={-(curr[2]-nxt[2]),curr[1]-nxt[1]}
-end
+  -- perpl
+  out[i]={-(curr[2]-nxt[2]),curr[1]-nxt[1]}
+ end
 
-return out
+ return out
 end
 
 -- get dot product for two vectors
@@ -21,20 +21,15 @@ function dot(a,b)return a[1]*b[1]+a[2]*b[2]end
 
 -- get projection for points
 function proj(axis, points)
-local mn=dot(axis,points[1])
-local mx=mn
+ local mn=dot(axis,points[1])
+ local mx=mn
 
-for i=1,#points do
-local p=dot(axis,points[i])
+ for i=1,#points do
+  local p=dot(axis,points[i])
+  if p<mn then mn=p elseif p>mx then mx=p end
+ end
 
-if p<mn then
-mn=p
-elseif p>mx then
-mx=p
-end
-end
-
-return {mn,mx}
+ return {mn,mx}
 end
 
 -- does vector a overlap vector b?
@@ -46,59 +41,41 @@ function get_overlap(a,b) return a[2]>b[1] and a[2]-b[1] or b[2]-a[1] end
 -- check collision between
 -- obj a and b
 function check(a,b)
-local a_offs,b_offs=offset_points(a),offset_points(b)
-local mtv,mtvlen={0,0},1
-local n=normals(a_offs)
+ local a_offs,b_offs=offset_points(a),offset_points(b)
+ local mtv,mtvlen={0,0},1
+ local n=normals(a_offs)
 
-for i=1,#n do
-local p1,p2=proj(n[i],a_offs),proj(n[i],b_offs)
+ for n in all(normals(a_offs),normals(b_offs)) do
+  for i=1,#n do
+   local p1,p2=proj(n[i],a_offs),proj(n[i],b_offs)
+ 
+   if not overlap(p1,p2)then
+    return false,mtv
+   else
+    local o=get_overlap(p1,p2)
+    if (mtvlen==-1 or o<mtvlen) mtvlen,mtv=o,n[i]
+   end
+  end
+ end
 
-if not overlap(p1,p2)then
-return false,mtv
-else
-local o=get_overlap(p1,p2)
-if mtvlen==-1 or o<mtvlen then
-mtvlen=o
-mtv=n[i]
-end
-end
-end
+ local len=sqrt(mtv[1]*mtv[1]+mtv[2]*mtv[2])
+ local nor={mtv[1]/len,mtv[2]/len}
 
-local n=normals(b_offs)
-
-for i=1,#n do
-local p1,p2=proj(n[i],a_offs),proj(n[i],b_offs)
-
-if not overlap(p1,p2)then
-return false,mtv
-else
-local o=get_overlap(p1,p2)
-if mtvlen==-1 or o<mtvlen then
-mtvlen=o
-mtv=n[i]
-end
-end
-end
-
-local len=sqrt(mtv[1]*mtv[1]+mtv[2]*mtv[2])
-local nor={mtv[1]/len,mtv[2]/len}
-local new={nor[1]*mtvlen,nor[2]*mtvlen}
-
-return true,new
+ return true,{nor[1]*mtvlen,nor[2]*mtvlen}
 end
 
 -- get the absolute points of an object
 function offset_points(obj)
-local offsets={}
+ local offsets={}
 
-for i=1,#obj.points do
--- todo: handle obj rotation
--- each point will need to be rotated by the rot
-offsets[i]={
-obj.pos[1]+obj.points[i][1],
-obj.pos[2]+obj.points[i][2]
-}
-end
+ for i=1,#obj.points do
+  -- todo: handle obj rotation
+  -- each point will need to be rotated by the rot
+  offsets[i]={
+  obj.pos[1]+obj.points[i][1],
+  obj.pos[2]+obj.points[i][2]
+  }
+ end
 
-return offsets
+ return offsets
 end
